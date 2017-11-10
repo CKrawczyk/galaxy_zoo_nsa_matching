78fbc2b310cc2bc6b167e076880b3a0e973ea346
CKrawczyk <coleman.krawczyk@gmail.com>
2017-11-10 17:23:38 +0000

    Rewrite lookup using SQL query
    This is much faster than the previous method.

--- ra_dec_lookup.py
+++ ra_dec_lookup.py
@@ -1,36 +1,32 @@
 from astroquery.sdss import SDSS
+from astropy.table import vstack
+import progressbar
+import pandas
+
+cmd = 'SELECT DISTINCT p.ra, p.dec, p.objid FROM PhotoObjAll AS p   WHERE p.objid in {0}'
+
+widgets = [
+    'Fetching: ',
+    progressbar.Percentage(),
+    ' ', progressbar.Bar(),
+    ' ', progressbar.ETA()
+]
 
 
-def objid_extract(obj_id, full=False):
-    masks = {
-        'sky_version': 0x7800000000000000,
-        'rerun': 0x07FF000000000000,
-        'run': 0x0000FFFF00000000,
-        'camcol': 0x00000000E0000000,
-        'first_field': 0x0000000010000000,
-        'field': 0x000000000FFF0000,
-        'id': 0x000000000000FFFF
-    }
-    run = (obj_id & masks['run']) >> 32
-    rerun = (obj_id & masks['rerun']) >> 48
-    camcol = (obj_id & masks['camcol']) >> 29
-    field = (obj_id & masks['field']) >> 16
-    id = (obj_id & masks['id']) >> 0
-    sky_version = (obj_id & masks['sky_version']) >> 59
-    first_field = (obj_id & masks['first_field']) >> 28
-    return {
-        'run': run,
-        'rerun': rerun,
-        'camcol': camcol,
-        'field': field,
-        'id': id,
-        'first_field': first_field,
-        'sky_version': sky_version
-    }
+def many_objid_lookup(obj_ids, chunk_size=300):
+    ids_chunks = [obj_ids[i:i + chunk_size] for i in range(0, len(obj_ids), chunk_size)]
+    table = []
+    pbar = progressbar.ProgressBar(widgets=widgets, max_value=len(ids_chunks))
+    pbar.start()
+    for idx, ids in enumerate(ids_chunks):
+        table.append(SDSS.query_sql(cmd.format(tuple(ids))))
+        pbar.update(idx+1)
+    pbar.finish()
+    return vstack(table)
 
 
-def objid_lookup(obj_id):
-    extract = objid_extract(obj_id)
-    results = SDSS.query_photoobj(run=extract['run'], camcol=extract['camcol'], field=extract['field'], rerun=extract['rerun'])
-    idx = results['objid'] == obj_id
-    return results[idx]
+gz4 = pandas.read_csv('gz4_aggregate_with_metadata.csv')
+fdx = ~pandas.isnull(gz4.sdss_id)
+dr8ids = pandas.np.array(gz4['sdss_id'][fdx]).astype(int).tolist()
+ra_dec_table = many_objid_lookup(dr8ids)
+ra_dec_table.write('dr8_ra_dec.csv', format='ascii.csv')
